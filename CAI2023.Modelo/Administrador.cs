32783fc9b63be70373abefa0c98cbdba01306e94
EmilianoMior <80928017+EmilianoMior@users.noreply.github.com>
2023-09-08 22:29:30 -0300

    Add files via upload

---
+++ CAI2023.Modelo/Administrador.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAI2023.Modelo
+{
+    class Administrador
+    {
+        public static void AltaDeUsuario(string nombre, string apellido, string nombreUsuario, string contraseña, bool esAdministrador)
+        {
+            if (nombreUsuario.Length < 8 || nombreUsuario.Length > 15)
+            {
+                Console.WriteLine("El nombre de usuario debe tener entre 8 y 15 caracteres.");
+                return;
+            }
+
+            if (nombreUsuario.Contains(nombre) || nombreUsuario.Contains(apellido))
+            {
+                Console.WriteLine("El nombre de usuario no puede contener ni el nombre ni el apellido del usuario.");
+                return;
+            }
+
+            if (!Regex.IsMatch(contraseña, @"^(?=.*[A-Z])(?=.*\d)(?!.*\s).{8,15}$"))
+            {
+                Console.WriteLine("La contraseña debe tener entre 8 y 15 caracteres, al menos una letra mayúscula y un número.");
+                return;
+            }
+
+            if (esAdministrador && usuarios.Exists(u => u.EsAdministrador))
+            {
+                Console.WriteLine("Ya existe un perfil de Administrador.");
+                return;
+            }
+            if (!esAdministrador)
+            {
+                // Generar una contraseña temporal para Supervisor y Vendedor
+                string contraseniaTemporal = GenerarContraseniaTemporal();
+                contraseñasTemporales[nombreUsuario] = contraseniaTemporal;
+                Console.WriteLine($"Contraseña temporal generada: {contraseniaTemporal}");
+            }
+
+            usuarios.Add(usuario);
+            Console.WriteLine("Usuario dado de alta correctamente.");
+
+        }
+        private static string GenerarContraseniaTemporal()
+        {
+            // Generar una contraseña temporal aleatoria con los requisitos necesarios
+            const string caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            Random random = new Random();
+            string contraseniaTemporal = new string(Enumerable.Repeat(caracteres, 8).Select(s => s[random.Next(s.Length)]).ToArray());
+            return contraseniaTemporal;
+        }
+
+
+
+
+    }
+}
---
+++ CAI2023.Modelo/Usuario.cs
@@ -0,0 +1,32 @@
+﻿namespace CAI2023.Modelo
+{
+    public class Usuario
+    {
+        public int UsuarioId { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Password { get; set; }
+        public string Perfil { get; set; }
+        public bool Activo { get; set; }
+        public bool CambiarPassword { get; set; }
+        public DateTime FechaPassword { get; set; }
+
+        public Usuario() { }
+
+        public Usuario(int usuarioId, string nombre, string apellido, string mail, string nombreUsuario, string password, string perfil, bool activo, bool cambiarPassword, DateTime fechaPassword)
+        {
+            UsuarioId = usuarioId;
+            Nombre = nombre;
+            Apellido = apellido;
+            Email = mail;
+            NombreUsuario = nombreUsuario;
+            Password = password;
+            Perfil = perfil;
+            Activo = activo;
+            CambiarPassword = cambiarPassword;
+            FechaPassword = fechaPassword;
+        }
+    }
+}
